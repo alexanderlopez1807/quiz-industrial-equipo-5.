623131f2c6fc2e2c055f857575665ce36412e6e5
alexanderlopez1807 <2503230730@cesunbc.edu.mx>
2026-02-05 00:32:04 -0800

    app.py

--- app.py
+++ app.py
@@ -106,7 +106,7 @@ if "pantalla" not in st.session_state:
     st.session_state.indice = 0
     st.session_state.puntos = 0
 
-st.title("🎮 Juego de Mejora Continua")
+st.title("preguntas sobre Procesos y Mejora Continua")
 st.write("Ingeniería Industrial")
 
 # -----------------------------
